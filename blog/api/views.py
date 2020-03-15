fa7231c65a1729f1435a86d3ec2b71aeeea96a3a
Cyberist-Edgar <201648748@qq.com>
2020-03-15 09:03:42 +0800

    添加新的应用api，并且实现邮箱的api调用

---
+++ blog/api/views.py
@@ -0,0 +1,27 @@
+from django.core.mail import send_mail
+from django.shortcuts import render
+from django.conf import settings
+from django.views.decorators.csrf import csrf_protect
+from django.http import JsonResponse
+import random
+
+
+# Create your views here.
+
+
+@csrf_protect
+def email(request):
+    if request.method == 'POST':
+        to = request.POST.get("email")
+        subject = request.POST.get("subject")
+        rand = random.randint(300000, 999999)
+        content = f"本次验证码为 {rand}  如果不是本人操作，请忽略"
+        data = {"status": "success", "code": rand}
+        try:
+            send_mail(subject, from_email=settings.DEFAULT_FROM_EMAIL, message=content, recipient_list=[to])
+        except:
+            data["status"] = "fail"
+            data["code"] = None
+        return JsonResponse(data)
+    else:
+        return render(request, 'email.html', locals())
