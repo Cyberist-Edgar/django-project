b0e9fa7d3f267f2fcf43c1ce13fc82bc96206057
Cyberist <47896650+Cyberist-Edgar@users.noreply.github.com>
2020-03-29 13:27:04 +0800

    Update settings.py

--- bbs/bbs/settings.py
+++ bbs/bbs/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'o4&wb0v9)fzcevfsyco!%4+f&t!*%-@ol!m_%l^f14arhvq8n3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
